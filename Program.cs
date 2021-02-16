d24ebd119722399b3cf74e5bf60892f4dc0ede07
sdp256 <steevnpairy@gmail.com>
2021-02-15 19:58:27 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Area_of_a_circle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const double pi = 3.14;
+            //double radius;
+            //Console.WriteLine("What is the radius of your circle?");
+
+            //radius = Convert.ToDouble(Console.ReadLine());
+
+            //double circleArea;
+
+
+            //circleArea = (radius * radius) * pi;
+
+            //Console.WriteLine($"The area of your circle with a radius of {radius} is {circleArea}");
+
+            var i = 3;
+            var j = 4;
+            var k = ++i * j++;
+            Console.WriteLine(k);
+        }
+    }
+}
